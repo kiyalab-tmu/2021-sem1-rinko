3482f2e54b97ae67efb180c64b3fe0ecd3f07b5f
jimar884 <60651943+jimar884@users.noreply.github.com>
2021-05-18 09:48:02 +0900

    edit q2 in chapter3

--- jimar884/chapter3/test_function.py
+++ jimar884/chapter3/test_function.py
@@ -1,9 +1,21 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-a = [1,2 ,3 ,4, 5, 6]
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 2, 1)
-ax1.plot(a)
-ax2 = fig.add_subplot(1, 2, 2)
-ax2.plot(a)
-plt.show()
+# a = [1,2 ,3 ,4, 5, 6]
+# fig = plt.figure()
+# ax1 = fig.add_subplot(1, 2, 1)
+# ax1.plot(a)
+# ax2 = fig.add_subplot(1, 2, 2)
+# ax2.plot(a)
+# plt.show()
+
+import numpy as np
+
+# cross entropy
+def cross_entropy(y, y_hat):
+    return -np.mean(np.log(y))
+
+a = np.array([1, 2, 3, 4, 5])
+b = a + 0.01
+print(a)
+print(b)
+print(cross_entropy(a, b))
