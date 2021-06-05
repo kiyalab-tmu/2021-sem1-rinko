89944ab47d42c17740616651be91a590ca52fc80
teru77 <teru-ngmr0715@docomo.ne.jp>
2021-06-05 19:39:37 +0900

    fix: Chapter4

--- teru77/Chapter4/Q1_to_Q5.py
+++ teru77/Chapter4/Q1_to_Q5.py
@@ -2,6 +2,7 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.lib.stride_tricks import as_strided
 from PIL import Image
 import torch.nn as nn
 import torch.nn.functional as F
@@ -52,39 +53,74 @@ class Q4(nn.Module):
 class Q5(nn.Module):
     def __init__(self):
         super().__init__() #nn.Moduleを継承
-        self.Maxpool = nn.MaxPool2d(2,2) #Q5(maxプーリング層)
-        self.Avgpool = nn.AvgPool2d(2,2) #Q5(averageプーリング層)    
+        self.Maxpool = nn.MaxPool2d(kernel_size = (8,8),stride=1) #Q5(maxプーリング層)
+        self.Avgpool = nn.AvgPool2d(kernel_size = (8,8),stride=1) #Q5(averageプーリング層)    
     def forward(self, x):
         Max_pooling =  self.Maxpool(x)
         Avg_pooling =  self.Avgpool(x)
         return Max_pooling,Avg_pooling
+ 
+def pooling(original_img,kernel_size,stride,padding,method="max"):
+    img = original_img.copy()
+    """
+    Parameters:
+        original_img: input 2D array
+        kernel_size: int, the size of the window
+        stride: int, the stride of the window
+        padding: int, implicit zero paddings on both sides of the input
+        method: string, 'max' or 'avg'
+    """
+    # Padding
+    img = np.pad(img, padding, mode='constant')
 
-img = Image.open("./imori.jpg")
-array = np.asarray(img, np.float32).transpose([2, 0, 1]) / 255.0
-tensor = torch.as_tensor(np.expand_dims(array, axis=0)) 
+    output_shape = ((img.shape[0] - kernel_size)//stride + 1,
+                    (img.shape[1] - kernel_size)//stride + 1)
+    kernel_size = (kernel_size, kernel_size)
+    img_w = as_strided(img,shape = output_shape + kernel_size, 
+                        strides = (stride*img.strides[0],
+                                   stride*img.strides[1]) + img.strides)
+    img_w = img_w.reshape(-1, *kernel_size)
+    
+    if method== 'max':
+        return img_w.max(axis=(1,2)).reshape(output_shape)
+    elif method == 'avg':
+        return img_w.mean(axis=(1,2)).reshape(output_shape)
 
-#Q1
-q1 = Q1() 
-output1 = q1(tensor)
-torchvision.utils.save_image(output1[0,:,:,:], "Q1.jpg")
+if __name__ == '__main__':
+    img = Image.open("./imori.jpg")
+    array = np.asarray(img, np.float32).transpose([2, 0, 1]) / 255.0
+    tensor = torch.as_tensor(np.expand_dims(array, axis=0)) 
 
-#Q2
-q2 = Q2() 
-output2 = q2(tensor)
-torchvision.utils.save_image(output2[0,:,:,:], "Q2.jpg")
+    #Q1
+    q1 = Q1() 
+    output1 = q1(tensor)
+    torchvision.utils.save_image(output1[0,:,:,:], "Q1.jpg")
 
-#Q3
-q3 = Q3() 
-output3 = q3(tensor)
-torchvision.utils.save_image(output3[0,:,:,:], "Q3.jpg")
+    #Q2
+    q2 = Q2() 
+    output2 = q2(tensor)
+    torchvision.utils.save_image(output2[0,:,:,:], "Q2.jpg")
 
-#Q4
-q4 = Q4() 
-output4 = q4(tensor)
-torchvision.utils.save_image(output4[0,:,:,:], "Q4.jpg")
+    #Q3
+    q3 = Q3() 
+    output3 = q3(tensor)
+    torchvision.utils.save_image(output3[0,:,:,:], "Q3.jpg")
 
-#Q5
-q5 = Q5() 
-Max_pooling,Avg_pooling = q5(tensor)
-torchvision.utils.save_image(Max_pooling[0,:,:,:], "Max_pooling.jpg")
-torchvision.utils.save_image(Avg_pooling[0,:,:,:], "Avg_pooling.jpg")
+    #Q4
+    q4 = Q4() 
+    output4 = q4(tensor)
+    torchvision.utils.save_image(output4[0,:,:,:], "Q4.jpg")
+
+    #Q5
+    q5 = Q5() 
+    Max_pooling,Avg_pooling = q5(tensor)
+    torchvision.utils.save_image(Max_pooling[0,:,:,:], "Max_pooling.jpg")
+    torchvision.utils.save_image(Avg_pooling[0,:,:,:], "Avg_pooling.jpg")
+
+    #numpy
+    A = np.random.randint(0,10,(4,4))
+    print(A)
+    max_pooling = pooling(A,2,1,0,method="max")
+    avg_pooling = pooling(A,2,1,0,method="avg") 
+    print(max_pooling)      
+    print(avg_pooling) 
