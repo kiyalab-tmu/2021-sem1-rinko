5543a426f0329081fd4791f9f8678590212ccf20
w-tomoka <82037886+w-tomoka@users.noreply.github.com>
2021-06-05 22:05:45 +0900

    update 4-2

--- w-tomoka/chapter4/4-2.py
+++ w-tomoka/chapter4/4-2.py
@@ -4,22 +4,20 @@ from skimage import io
 import cv2
 
 def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
-    H, W= input_data.shape
-    N = 1
-    C = 1
+    N, C, H, W= input_data.shape
     out_h = (H + 2*pad - filter_h)//stride + 1
     out_w = (W + 2*pad - filter_w)//stride + 1
 
-    img = np.pad(input_data, [(pad, pad), (pad, pad)], 'constant')
-    col = np.zeros((N,filter_h, filter_w, out_h, out_w))
+    img = np.pad(input_data, [(0, 0), (0, 0), (pad, pad), (pad, pad)], 'constant')
+    col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
 
     for y in range(filter_h):
         y_max = y + stride*out_h
         for x in range(filter_w):
             x_max = x + stride*out_w
-            col[:,y, x, :, :] = img[y:y_max:stride, x:x_max:stride]
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
 
-    col = col.transpose(0, 3, 4, 1, 2).reshape(N*out_h*out_w, -1)
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
     return col
 
 
@@ -44,9 +42,7 @@ class Convolution:
         #各データに関するサイズを取得
         FH, FW = self.W.shape #フィルター
         FN = 1
-        H, W= x.shape #入力データ
-        N = 1
-        C = 1
+        N, C, H, W= x.shape #入力データ
         out_h = int(1 + (H + 2*self.pad - FH) / self.stride) #出力データ
         out_w = int(1 + (W + 2*self.pad - FW) / self.stride)
 
@@ -58,12 +54,18 @@ class Convolution:
         out = np.dot(col, col_W) + self.b
         out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
 
+        #(逆伝播時に使用する)中間データを保存
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
         return out
 
 
 img_ori = io.imread('https://yoyoyo-yo.github.io/Gasyori100knock/dataset/images/imori_256x256.png')
-img_gray = cv2.cvtColor(img_ori, cv2.COLOR_RGB2GRAY)
-img = np.array(img_gray)
+img_gray = cv2.cvtColor(img_ori, cv2.COLOR_BGR2GRAY)
+img = np.array(img_gray)[np.newaxis, np.newaxis]
+print(img.shape)
 
 #W = np.array([[1, 0, -1],[0, 0, 0],[-1,0,1]])
 #W = np.array([[0,-1,0],[-1,4,-1],[0,-1,0]])
@@ -72,14 +74,12 @@ b = 0
 conv = Convolution(W, b, stride=1, pad=0)
 Y = conv.forward(img)
 print(Y.shape)
-Y = Y[0][0][:][:]
-print(Y.shape)
 
 plt.figure(figsize=(12, 3))
 plt.subplot(1, 2, 1)
 plt.title('input')
-plt.imshow(img, cmap='gray')
+plt.imshow(img_gray, cmap='gray')
 plt.subplot(1, 2, 2)
 plt.title('answer')
-plt.imshow(Y, cmap='gray')
+plt.imshow(Y[0][0], cmap='gray')
 plt.show()
