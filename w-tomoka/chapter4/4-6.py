17889402de8e59e7b3491be3cbec6c4fbbe46de8
w-tomoka <82037886+w-tomoka@users.noreply.github.com>
2021-06-10 20:09:41 +0900

    Create 4-6.py

---
+++ w-tomoka/chapter4/4-6.py
@@ -0,0 +1,137 @@
+import torch
+import torchvision
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+class LeNet(nn.Module):
+  def __init__(self):
+    super(LeNet, self).__init__()
+    self.conv1 = nn.Conv2d(1, 6, kernel_size=5, padding=2)
+    self.conv2 = nn.Conv2d(6, 16, kernel_size=5, padding=0)
+    self.fc1 = nn.Linear(5*5*16, 120)
+    self.fc2 = nn.Linear(120, 84)
+    self.fc3 = nn.Linear(84, 10)
+    self.pool = nn.AvgPool2d(2, stride=2)
+
+  def forward(self, x):
+    x = F.sigmoid(self.conv1(x))
+    x = self.pool(x)
+    x = F.sigmoid(self.conv2(x))
+    x = self.pool(x)
+    x = x.view(-1, 5*5*16)
+    x = F.sigmoid(self.fc1(x))
+    x = F.sigmoid(self.fc2(x))
+    x = self.fc3(x)
+    return x
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+#dataset
+BATCH_SIZE = 500
+transform = torchvision.transforms.ToTensor()
+#60,000
+fashion_mnist_data = torchvision.datasets.MNIST(
+    root = './fashion-mnist', 
+    train = True, 
+    download = True, 
+    transform = transform)
+data_loader = torch.utils.data.DataLoader(
+    dataset=fashion_mnist_data,
+    batch_size=BATCH_SIZE,
+    shuffle=True)
+
+#10,000
+fashion_mnist_data_test = torchvision.datasets.MNIST(
+    root = './fashion-mnist', 
+    train = False, 
+    download = True, 
+    transform = transform)
+data_loader_test = torch.utils.data.DataLoader(
+    dataset=fashion_mnist_data,
+    batch_size=BATCH_SIZE,
+    shuffle=False)
+
+#学習
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+net = LeNet()
+net = net.to(device)
+optimizer = optim.SGD(net.parameters(), lr=0.01)
+criterion = nn.CrossEntropyLoss()
+
+train_losses = []
+train_accs = []
+test_losses = []
+test_accs = []
+
+epochs = 40
+for epoch in range(epochs):
+  #学習ループ
+  running_loss = 0.0
+  running_acc = 0.0
+  for x, labels in data_loader:
+    x = x.to(device)
+    labels = labels.to(device)
+    optimizer.zero_grad()
+    output = net(x)
+    loss = criterion(output, labels)
+    loss.backward()
+    running_loss += loss.item()
+    pred = torch.argmax(output, dim=1)
+    running_acc += (pred == labels).sum().item()
+    optimizer.step()
+
+  running_loss /= len(data_loader)
+  running_acc = float(running_acc / len(fashion_mnist_data))
+  train_losses.append(running_loss)
+  train_accs.append(running_acc)
+
+  test_loss = 0.0
+  test_acc = 0.0
+  for x, labels in data_loader_test:
+    x = x.to(device)
+    labels = labels.to(device)
+    output = net(x)
+    loss = criterion(output, labels)
+    test_loss += loss.item()
+    pred = torch.argmax(output, dim=1)
+    test_acc += (pred == labels).sum().item()
+  test_loss /= len(data_loader_test)
+  test_acc = float(test_acc / len(fashion_mnist_data_test))
+  test_losses.append(test_loss)
+  test_accs.append(test_acc)
+
+  print(f'epoch:{epoch}, '
+        f'train_loss: {running_loss:.3f}, train_acc: {running_acc:.3f}, '
+        f'test_loss: {test_loss:.3f}, test_acc{test_acc:.3f}')
+  
+
+fig, ax = plt.subplots(2)
+ax[0].plot(train_losses, label='train loss')
+ax[0].plot(test_losses, label='test loss')
+ax[0].legend()
+ax[1].plot(train_accs, label='train acc')
+ax[1].plot(test_accs, label='test acc')
+ax[1].legend()
+plt.show()
