cdecaf7aa90968bda3fe3182fa205f4d504cbf5f
Hiroki Ito <i.hiroki0307@icloud.com>
2021-06-26 17:47:14 +0900

    Add add preprocess code

---
+++ ito-hiroki/chap5/preprocess.py
@@ -0,0 +1,29 @@
+from collections import Counter
+from pathlib import Path
+
+import pandas as pd
+
+
+def line_split(line, base="word"):
+    if base == "word":
+        ans = line.split()
+    elif base == "char":
+        ans = list(line)
+    return ans
+
+
+if __name__ == "__main__":
+    split_rule = "char"
+    save_path = Path("../data/TheTimeMachine/")
+    save_path.mkdir(parents=True, exist_ok=True)
+
+    counter = Counter()
+    with open(save_path / "35-0.txt", "r", encoding="utf-8-sig") as file:
+        lines = file.readlines()
+
+    for line in lines:
+        counter.update(line_split(line.strip(), base=split_rule))
+
+    pairs = [("<EOS>", 0), ("<UNK>", 0)] + counter.most_common()
+    result_df = pd.DataFrame(pairs, columns=["word", "count"])
+    result_df.to_csv(save_path / "{}.csv".format(split_rule))
