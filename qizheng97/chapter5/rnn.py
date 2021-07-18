1ff93c402370f9c48d45c5328b336fc1c6d399bb
MINIQ1 <78834818+MINIQ1@users.noreply.github.com>
2021-07-19 01:31:51 +0900

    chapter5

---
+++ qizheng97/chapter5/rnn.py
@@ -0,0 +1,129 @@
+import torch
+from torch import nn
+import re
+import collections
+import math
+import sys
+import time
+import random
+def one_hot(x, n_class, dtype=torch.float32):
+    result = torch.zeros(
+        x.shape[0],
+        n_class,
+        dtype=dtype,
+        device=x.device)  # shape: (n, n_class)
+    result.scatter_(1, x.long().view(-1, 1), 1)  # result[i, x[i, 0]] = 1
+    return result
+
+def to_onehot(X, n_class):
+    return [one_hot(X[:, i], n_class) for i in range(X.shape[1])]
+
+class RNNModel(nn.Module):
+    def __init__(self, rnn_layer, vocab_size):
+        super(RNNModel, self).__init__()
+        self.rnn = rnn_layer
+
+        self.hidden_size = rnn_layer.hidden_size * (2 if rnn_layer.bidirectional else 1)
+        self.vocab_size = vocab_size
+        self.dense = nn.Linear(self.hidden_size, vocab_size)
+
+    def forward(self, inputs, state):
+        # inputs.shape: (batch_size, num_steps)
+        X = to_onehot(inputs, self.vocab_size)
+        X = torch.stack(X)  # X.shape: (num_steps, batch_size, vocab_size)
+        hiddens, state = self.rnn(X, state)
+        hiddens = hiddens.view(-1, hiddens.shape[-1])  # hiddens.shape: (num_steps * batch_size, hidden_size)
+        output = self.dense(hiddens)
+        return output, state
+
+
+def data_iter_random(corpus_indices, batch_size, num_steps, device=None):
+    # 减1是因为对于长度为n的序列，X最多只有包含其中的前n - 1个字符
+    num_examples = (len(corpus_indices) - 1) // num_steps  # 下取整，得到不重叠情况下的样本个数
+    example_indices = [i * num_steps for i in range(num_examples)]  # 每个样本的第一个字符在corpus_indices中的下标
+    random.shuffle(example_indices)
+
+    def _data(i):
+        # 返回从i开始的长为num_steps的序列
+        return corpus_indices[i: i + num_steps]
+
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    for i in range(0, num_examples, batch_size):
+        # 每次选出batch_size个随机样本
+        batch_indices = example_indices[i: i + batch_size]  # 当前batch的各个样本的首字符的下标
+        X = [_data(j) for j in batch_indices]
+        Y = [_data(j + 1) for j in batch_indices]
+        yield torch.tensor(X, device=device), torch.tensor(Y, device=device)
+
+def data_iter_consecutive(corpus_indices, batch_size, num_steps, device=None):
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    corpus_len = len(corpus_indices) // batch_size * batch_size  # 保留下来的序列的长度
+    corpus_indices = corpus_indices[: corpus_len]  # 仅保留前corpus_len个字符
+    indices = torch.tensor(corpus_indices, device=device)
+    indices = indices.view(batch_size, -1)  # resize成(batch_size, )
+    batch_num = (indices.shape[1] - 1) // num_steps
+    for i in range(batch_num):
+        i = i * num_steps
+        X = indices[:, i: i + num_steps]
+        Y = indices[:, i + 1: i + num_steps + 1]
+        yield X, Y
+def predict_rnn_pytorch(prefix, num_chars, model, vocab_size, device, idx_to_char,
+                        char_to_idx):
+    state = None
+    output = [char_to_idx[prefix[0]]]  # output记录prefix加上预测的num_chars个字符
+    for t in range(num_chars + len(prefix) - 1):
+        X = torch.tensor([output[-1]], device=device).view(1, 1)
+        (Y, state) = model(X, state)  # 前向计算不需要传入模型参数
+        if t < len(prefix) - 1:
+            output.append(char_to_idx[prefix[t + 1]])
+        else:
+            output.append(Y.argmax(dim=1).item())
+    return ''.join([idx_to_char[i] for i in output])
+
+
+def train_and_predict_rnn_pytorch(model, num_hiddens, vocab_size, device,
+                                  corpus_indices, idx_to_char, char_to_idx,
+                                  num_epochs, num_steps, lr, clipping_theta,
+                                  batch_size, pred_period, pred_len, prefixes):
+    loss = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    model.to(device)
+    lossrecord,perplexity=[],[]
+    for epoch in range(num_epochs):
+        l_sum, n, start = 0.0, 0, time.time()
+        data_iter = data_iter_consecutive(corpus_indices, batch_size, num_steps, device)  # 相邻采样
+        state = None
+        lossre=0
+        for X, Y in data_iter:
+            if state is not None:
+                # 使用detach函数从计算图分离隐藏状态
+                if isinstance(state, tuple):  # LSTM, state:(h, c)
+                    state[0].detach_()
+                    state[1].detach_()
+                else:
+                    state.detach_()
+            (output, state) = model(X, state)  # output.shape: (num_steps * batch_size, vocab_size)
+            y = torch.flatten(Y.T)
+            l = loss(output, y.long())
+            lossre+=l.item()
+            optimizer.zero_grad()
+            l.backward()
+            #grad_clipping(model.parameters(), clipping_theta, device)
+            optimizer.step()
+            l_sum += l.item() * y.shape[0]
+            n += y.shape[0]
+
+        lossrecord.append(lossre/batch_size)
+        #print(lossre/batch_size)
+        perplexity.append(math.exp(l_sum / n))
+        if (epoch + 1) % pred_period == 0:
+            print('epoch %d, perplexity %f, time %.2f sec' % (
+                epoch + 1, math.exp(l_sum / n), time.time() - start))
+            for prefix in prefixes:
+                print(' -', predict_rnn_pytorch(
+                    prefix, pred_len, model, vocab_size, device, idx_to_char,
+                    char_to_idx))
+    return lossrecord,perplexity
